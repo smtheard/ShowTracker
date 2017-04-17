349534c4370684c772c02f62fb7953c38135de90
Stefan Theard <smtheard@gmail.com>
2017-04-16 19:51:28 -0500

    add import for pytvmaze lib

--- models/tv_maze_api.py
+++ models/tv_maze_api.py
@@ -3,6 +3,8 @@ from models.episode import Episode
 from models.network import Network
 from models.genre import Genre
 
+import pytvmaze
+
 class TVMazeAPI():
   def hello(self):
     print "hello world"

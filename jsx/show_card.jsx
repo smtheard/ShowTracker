28072670cbbaf5f2a6d0d376b3f2976c877093f6
Stefan Theard <smtheard@gmail.com>
2017-04-23 22:52:27 -0500

    make width a prop, wrap header in <a>

--- jsx/show_card.jsx
+++ jsx/show_card.jsx
@@ -23,25 +23,21 @@ var ShowCard = React.createClass({
     this.setState({opacity: 3});
   },
 
-  viewShow: function(event) {
-    // TODO: implement url (going to need slugs)
-    window.location = this.props.url;
-  },
-
   render: function() {
     return (
       <div className="mdl-card mdl-shadow--2dp" 
-           style={{height: "340px", width: "370px", display: "inline-block", margin: "5px"}}>
-        <div className="mdl-card__title"
-             style={{ cursor: "pointer", position: "relative", padding: 0, color: "#fff", height: "176px", background: "url('" + this.props.image_src + "')" }}
-             onMouseOver={this.increaseOpacity}
-             onMouseLeave={this.decreaseOpacity}
-             onClick={this.viewShow}>
-          <div 
-               style={{position: "absolute", background: "rgba(0, 0, 0, 0."+ this.state.opacity + ")", width: "470px", bottom: 0, padding: "10px"}}>
-            <h2 className="mdl-card__title-text">{this.props.title}</h2>
+           style={{height: "340px", width: this.props.width, display: "inline-block", margin: "5px"}}>
+        <a href={this.props.path}>
+          <div className="mdl-card__title"
+               style={{ cursor: "pointer", position: "relative", padding: 0, color: "#fff", height: "176px", background: "url('" + this.props.image_src + "')" }}
+               onMouseOver={this.increaseOpacity}
+               onMouseLeave={this.decreaseOpacity}>
+            <div 
+                 style={{position: "absolute", background: "rgba(0, 0, 0, 0."+ this.state.opacity + ")", width: this.props.width, bottom: 0, padding: "10px"}}>
+              <h2 className="mdl-card__title-text">{this.props.title}</h2>
+            </div>
           </div>
-        </div>
+        </a>
         <div onClick={this.toggleDesc} className="mdl-card__supporting-text" style={{height: "75px", overflowY: "auto"}}>
           {this.state.expanded ? this.props.description : this.truncate(this.props.description)}
         </div>

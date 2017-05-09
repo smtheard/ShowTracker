40a2e6c75283990969581675abf3f72eb7b2a176
Stefan Theard <smtheard@gmail.com>
2017-05-09 07:40:10 -0500

    backend implementation for show follows

--- controllers/rest.py
+++ controllers/rest.py
@@ -1,3 +1,4 @@
+import bottle
 from config import app, br, sa_session
 
 from models.user import User
@@ -22,7 +23,29 @@ from models.show_follow import ShowFollow
 @app.get("/rest/show-follows/<show_id>")
 def show_follow(session, show_id):
   if(session["user_id"]):
-    show_follow = sa_session.query(ShowFollow).filter(Show.id==show_id).filter(User.id==session["user_id"]).first()
+    show_follow = sa_session.query(ShowFollow) \
+                            .filter(ShowFollow.show_id==show_id, ShowFollow.user_id==session["user_id"]) \
+                            .first()
     return dict(following=(show_follow is not None), success=True)
   else:
     return dict(following=False, success=True)
+
+@app.post("/rest/show-follows/<show_id>")
+def update_show_follow(session, show_id):
+  following = bottle.request.json["following"]
+  user_id = session["user_id"]
+  if(user_id):
+    if(following):
+      sa_session.query(ShowFollow) \
+                .filter(ShowFollow.show_id==show_id, ShowFollow.user_id == user_id) \
+                .delete()
+      return dict(following=False, success=True)
+    else:
+      show_follow = ShowFollow(show_id=show_id, user_id=user_id)
+      sa_session.add(show_follow)
+      sa_session.commit()
+      return dict(following=True, success=True)
+  else:
+    raise "guest users can't follow shows"
+
+

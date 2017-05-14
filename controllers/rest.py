b9306283f0c3a1cff1c67cd874f45a1b51227195
Stefan Theard <smtheard@gmail.com>
2017-05-13 19:07:08 -0500

    season watch button implemented

--- controllers/rest.py
+++ controllers/rest.py
@@ -151,3 +151,27 @@ def season_watch(session, show_id, season_number):
     return dict(watched=(episodes_watched == episode_count), success=True)
   else:
     return dict(watched=False, success=True)
+
+@app.post("/rest/season-watch/<show_id>/<season_number>")
+def update_season_watch(session, show_id, season_number):
+  watched = bottle.request.json["watched"]
+  user_id = session["user_id"]
+  if(user_id):
+    episode_watches = sa_session.query(EpisodeWatch) \
+              .filter(EpisodeWatch.user_id == user_id) \
+              .join(EpisodeWatch.episodes) \
+              .filter(Episode.show_id == show_id) \
+              .filter(Episode.season == season_number) \
+              .all()
+    episode_watch_ids = map(lambda ew: ew.id, episode_watches)
+    sa_session.query(EpisodeWatch).filter(EpisodeWatch.id.in_(episode_watch_ids)).delete(synchronize_session='fetch')
+    sa_session.commit()
+    if(watched):
+      return dict(watched=False, success=True)
+    else:
+      episodes = sa_session.query(Episode).filter(Episode.show_id == show_id).filter(Episode.season == season_number).all()
+      for episode in episodes:
+        episode_watch = EpisodeWatch(episode_id=episode.id, user_id=user_id)
+        sa_session.add(episode_watch)
+      sa_session.commit()
+      return dict(watched=True, success=True)

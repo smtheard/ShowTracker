9dbf14b799245b495699a8369167df5850db732c
Stefan Theard <smtheard@gmail.com>
2017-05-20 19:09:45 -0500

    separate user being viewed from the current user

--- controllers/user.py
+++ controllers/user.py
@@ -4,22 +4,32 @@ from config import app, br, sa_session
 from models.show import Show
 from models.user import User
 from models.show_follow import ShowFollow
+from models.episode import Episode
 
 from collections import defaultdict
 
 @app.get('/user/<slug>')
 def user(session, slug):
-  user = None
-  user_id = session.get("user_id")
-  if(user_id):
-    user = sa_session.query(User).filter(User.id == user_id).first()
+  current_user = None
+  current_user_id = session.get("user_id")
+  if(current_user_id):
+    current_user = sa_session.query(User).filter(User.id == current_user_id).first()
+
+  user_being_viewed = sa_session.query(User).filter(User.slug == slug).first()
 
   followed_shows = sa_session.query(Show) \
     .join(Show.show_follows) \
-    .filter(ShowFollow.user_id==user_id)
+    .filter(ShowFollow.user_id==user_being_viewed.id) \
+    .options(joinedload("show_follows"))
 
-  props = user.to_page_dict()
-  props["followed_shows"] = map(lambda show: show.to_card_dict(user), followed_shows)
+  recent_episodes = sa_session.query(Episode) \
+    .filter(Episode.show_id.in_(map(lambda show: show.id, followed_shows))) \
+    .options(joinedload("episode_watches")) \
+    .options(joinedload("show"))
+
+  props = user_being_viewed.to_page_dict()
+  props["followed_shows"] = map(lambda show: show.to_card_dict(user_being_viewed), followed_shows)
+  props["episodes"] = map(lambda episode: episode.to_dict(current_user), recent_episodes)
   return br.render_html(
     br.BaseLayout({}, [
       br.UserPage(props)

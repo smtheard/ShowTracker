b214c6b7668cfb462fc7d1911fd64834fb9ac199
Stefan Theard <smtheard@gmail.com>
2017-05-21 16:44:03 -0500

    update nav menu for guest users

--- jsx/base_layout.jsx
+++ jsx/base_layout.jsx
@@ -1,4 +1,8 @@
 var BaseLayout = React.createClass({
+  componentDidMount: function() {
+    componentHandler.upgradeDom();
+  },
+
   render: function() {
     return (
       <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header" style={{alignItems: "center"}}>
@@ -6,13 +10,30 @@ var BaseLayout = React.createClass({
           <div className="mdl-layout__header-row">
             <a href="/" style={{textDecoration: "none", color: "white"}}><span className="mdl-layout-title">Overseer.TV</span></a>
             <div className="mdl-layout-spacer"></div>
-            <nav className="mdl-navigation mdl-layout--large-screen-only">
+            
             { this.props.current_user ? 
-              <a className="mdl-navigation__link" href={this.props.current_user.path}><i className="material-icons">&#xE853;</i></a>
+              <nav className="mdl-navigation">
+                <a className="mdl-navigation__link" href={this.props.current_user.path}><i className="material-icons">&#xE853;</i></a>
+                <button id="more-menu"
+                        className="mdl-button mdl-js-button mdl-button--icon">
+                  <i className="material-icons">&#xE5D4;</i>
+                </button>
+                <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+                    htmlFor="more-menu">
+                  <li onClick={_ => {location = "/settings";}} className="mdl-menu__item">Settings</li>
+                  <li onClick={_ => {location = "/logout";}} className="mdl-menu__item">Logout</li>
+                </ul>
+              </nav>
               :
-              <span/>
+              <nav className="mdl-navigation">
+                <button onClick={_ => {location = "/login";}} style={{marginRight: '10px'}}className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
+                  Login
+                </button>
+                <button onClick={_ => {location = "/register";}} className="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
+                  Register
+                </button>
+              </nav>
             }
-            </nav>
           </div>
         </header>
         <main style={{maxWidth: "1140px", padding: "24px"}}>

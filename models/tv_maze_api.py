e8871ab3b3b3215e087b0d1840f726659e5028e2
Stefan Theard <smtheard@gmail.com>
2017-05-24 00:03:44 -0500

    add refresh method, untested.

--- models/tv_maze_api.py
+++ models/tv_maze_api.py
@@ -69,6 +69,63 @@ class TVMazeAPI(object):
     sa_session.commit()
 
   @staticmethod
+  def refresh(show, tvm_id):
+    tvm_show = TVMazeAPI.tvm.get_show(show_name=show_name, maze_id=tvmaze_id)
+
+    show.thetvdb_id=tvm_show.externals and tvm_show.externals.get("thetvdb"),
+    show.tvrage_id=tvm_show.externals and tvm_show.externals.get("tvrage"),
+    show.imdb_id=tvm_show.externals and tvm_show.externals.get("imdb"),
+    show.tvmaze_id=tvm_show.maze_id,
+    show.title=tvm_show.name,
+    show.slug=sa_helper.generate_slug(sa_session, Show, slugify.slugify(tvm_show.name)),
+    show.description=tvm_show.summary,
+    show.tvmaze_img_src=tvm_show.image and tvm_show.image.get("original"),
+    show.tvmaze_rating=tvm_show.rating and tvm_show.rating.get("average"),
+    show.premiere_date=tvm_show.premiered,
+    show.schedule_days=' '.join(tvm_show.schedule and tvm_show.schedule.get("days") or []),
+    show.schedule_time=tvm_show.schedule and tvm_show.schedule.get("time"),
+    show.status=tvm_show.status,
+    show.tvmaze_url=tvm_show.url,
+    show.last_cached_at=datetime.utcnow(),
+    show.network_id=network and network.id,
+    show.tvmaze_updated_at=datetime.fromtimestamp(tvm_show.updated, utc)
+
+    episodes = show.episodes
+    episodes_by_tvm_id = {ep.tvmaze_id: ep for ep in episodes}
+    tvm_episodes = tvm_show.episodes
+
+    for tvm_episode in tvm_show.episodes:
+      ep = episodes_by_tvm_id.get(tvm_episode.maze_id)
+      if(ep):
+          ep.first_air=parse(tvm_episode.airstamp).astimezone(utc),
+          ep.number=tvm_episode.episode_number,
+          ep.tvmaze_id=tvm_episode.maze_id,
+          ep.season=tvm_episode.season_number,
+          ep.is_special=tvm_episode.special,
+          ep.description=tvm_episode.summary,
+          ep.title=tvm_episode.title,
+          ep.tvmaze_url=tvm_episode.url,
+          ep.last_cached_at=datetime.utcnow(),
+          ep.show_id=show.id
+      else:
+        new_ep = Episode(
+          first_air=parse(tvm_episode.airstamp).astimezone(utc),
+          number=tvm_episode.episode_number,
+          tvmaze_id=tvm_episode.maze_id,
+          season=tvm_episode.season_number,
+          is_special=tvm_episode.special,
+          description=tvm_episode.summary,
+          title=tvm_episode.title,
+          tvmaze_url=tvm_episode.url,
+          last_cached_at=datetime.utcnow(),
+          show_id=show.id
+          )
+        sa_session.add(new_ep)
+
+    sa_session.commit()
+
+
+  @staticmethod
   def sync_cache(re_cache_all=False):
     tvm_updates = pytvmaze.show_updates().updates
 

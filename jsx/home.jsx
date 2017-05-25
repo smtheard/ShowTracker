12a330db9785f7b2283347ca64de3124633fdfd0
Stefan Theard <smtheard@gmail.com>
2017-05-24 23:09:25 -0500

    implement search input and state

--- jsx/home.jsx
+++ jsx/home.jsx
@@ -2,11 +2,12 @@
 
 var Home = React.createClass({
   getInitialState: function() {
-    return {shows: [], page: 0};
+    return {shows: [], page: 0, query: ""};
   },
 
   componentWillMount: function() {
     this.loadShows();
+    componentHandler.upgradeDom();
   },
 
   loadShows: function() {
@@ -16,12 +17,12 @@ var Home = React.createClass({
        url: '/rest/shows',
        success: (data) => {
           if(data.success)
-            this.updateState(data.shows);
+            this.updateShows(data.shows);
         }
     });
   },
 
-  updateState: function(show_data) {
+  updateShows: function(show_data) {
     // data format:
     // [{id: 1, title: "Game of Thrones", image_src: "www.gameofthrones.com/image/5", ...},
     //  {id: 2, title: "Billions", ...}]
@@ -31,12 +32,22 @@ var Home = React.createClass({
     this.setState({shows: shows});
   },
 
+  updateQuery: function(event) {
+    this.setState({query: event.target.value});
+  },
+
   render: function() {
     var show_cards = this.state.shows.map(show => {
-      return (<bottlereact.ShowCard {...show} width="370"/>);
+      return (<bottlereact.ShowCard {...show} width="370px"/>);
     });
     return (
       <div>
+        <form action="#">
+          <div style={{width: "100%"}} className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input value={this.state.query} onChange={this.updateQuery} className="mdl-textfield__input" type="text" id="search" />
+            <label className="mdl-textfield__label" htmlFor="search">Search Shows...</label>
+          </div>
+        </form>
         {show_cards}
       </div>
     )
